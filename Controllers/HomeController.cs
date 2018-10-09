e17a2b82ff6272d5215114505ed4cd89625bc93b
Tyrone Moodley <TMoodley@equitable.ca>
2018-10-09 10:45:43 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace SampleMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // If the user is authenticated, then this is how you can get the access_token and id_token
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                string idToken = await HttpContext.GetTokenAsync("id_token");
+
+                var token = await GetTokenAsync();
+                var obj = await GetUserIdAsync(token, accessToken);
+                string userName = obj["username"];
+                HttpContext.Session.SetString("auth0UserName", userName);
+                var equitable_id = obj["user_metadata"];
+                string equitableId = equitable_id["equitable_id"];
+                HttpContext.Session.SetString("equitable_id", equitableId);
+                ViewBag.UserName = HttpContext.Session.GetString("auth0UserName");
+                ViewBag.EquitableId = equitableId;
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        { 
+            ViewBag.UserName = HttpContext.Session.GetString("auth0UserName"); 
+            var equitableId = HttpContext.Session.GetString("equitable_id");
+            ViewBag.EquitableId = equitableId;
+
+            return View();
+        }
+
+
+        private static async Task<dynamic> GetUserAsync(string userId, string token)
+        {
+            var client = new RestClient("https://mainsite.auth0.com/api/v2/users/" + userId);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer " + token);
+
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+
+            dynamic blogObject = JsonConvert.DeserializeObject(response.Content);
+           
+            return blogObject;
+        }
+
+        private static async Task<dynamic> GetUserIdAsync(string token, string accessToken)
+        {
+            var client = new RestClient("https://mainsite.auth0.com/userinfo");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer " + accessToken); 
+          
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+
+            dynamic blogObject = JsonConvert.DeserializeObject(response.Content);
+            string userId = blogObject["sub"];
+
+            Task<dynamic> obj = GetUserAsync(userId, token);
+            return await obj;
+        }
+          
+        public async Task<string> GetTokenAsync()
+        {
+            var client = new RestClient("https://mainsite.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            // Configure the Auth0 Client ID and Client Secret 
+            string client_id = Configuration["Auth0:ClientId"];
+            string client_secret = Configuration["Auth0:ClientSecret"];
+            request.AddParameter("application/json", "{\"client_id\":\"" + client_id
+                                + "\",\"client_secret\":\"" + client_secret
+                                + "\",\"audience\":\"https://mainsite.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+
+            dynamic blogObject = JsonConvert.DeserializeObject(response.Content);
+            string token = blogObject["access_token"];
+            return token;
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+using SampleMvcApp.Models;
+
+namespace SampleMvcApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public RegisterController(IConfiguration configuration)
+        {
+            Configuration = configuration; 
+        }
+        // GET: api/Regiser
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Regiser/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Register  
+        [HttpPost()]
+        public async Task Post([FromBody]User user)
+        {
+            // Get Token from auth0
+            string token = await GetTokenAsync();
+            // Register user with auth0
+            await RegisterUserAsync(user, token);
+        }
+
+        private static async Task RegisterUserAsync(User user, string token)
+        {
+            var clientToken = new RestClient("https://equitablelife.auth0.com/api/v2/users");
+            RestRequest request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer " + token);
+
+            request.AddParameter("application/json",
+                                "{\"email\": \"" + user.Email
+                                + "\",\"password\": \"" + user.Password
+                                + "\",\"username\": \"" + user.Username
+                                + "\",\"name\": \"" + user.Name
+                                + "\",\"connection\": \"" + user.Connection
+                                + "\", \"user_metadata\": {\"type\": \"plan-member\"}, \"app_metadata\": {\"equitableId\": \"132456\"}}",
+                                ParameterType.RequestBody);
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            RestRequestAsyncHandle handle = clientToken.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+        }
+
+        public async Task<string> GetTokenAsync()
+        {
+            var client = new RestClient("https://equitablelife.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            // Configure the Auth0 Client ID and Client Secret 
+            string client_id = Configuration["Auth0:ClientId"];
+            string client_secret = Configuration["Auth0:ClientSecret"];
+            request.AddParameter("application/json", "{\"client_id\":\"" + client_id
+                                + "\",\"client_secret\":\"" + client_secret
+                                + "\",\"audience\":\"https://equitablelife.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+
+            dynamic blogObject = JsonConvert.DeserializeObject(response.Content);
+            string token = blogObject["access_token"];
+            return token;
+        }
+
+        // PUT: api/Regiser/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
